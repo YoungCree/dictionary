b3fb0fc88da3c0b6670727dbbfb50a2aad607550
Corey Devenport <corey@thedevenports.com>
2021-02-22 22:29:47 -0700

    Added random gif functionality, added hover info and animations

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.getElementById("wordSubmit").addEventListener("click", function(event) 
   console.log(value);
   var trend = document.getElementById("trendingResults");
   trend.style.display = "none";
+  document.getElementById("randomResults").style.display = "none";
 
   const url = "https://api.giphy.com/v1/gifs/search?" +
     "api_key=" + api_key +
@@ -25,14 +26,18 @@ document.getElementById("wordSubmit").addEventListener("click", function(event) 
       results += "<div class='row justify-content-center'>";
 
       for (let i = 0; i < json.data.length; i++) {
-        results += "<div class='col-lg my-auto' align='center'>";
+        results += "<div class='col-lg my-auto img-box' align='center'>";
         results += "<img class='img-search-result' src='" + json.data[i].images.fixed_width.url + "'></img>";
+        results += "<div class='overlay'>";
+        results += "<div class='img-overlay-txt'>" + json.data[i].title + "</div>";
+        results += "</div>";
         results += "</div>";
       }
 
       results += "</div>";
       results += "</div>";
       document.getElementById("searchResults").innerHTML = results;
+      document.getElementById("searchResults").style.display = "block";
 
     }).then(function() {
       var foot = document.getElementById("footer");
@@ -57,8 +62,11 @@ function loadTrending() {
       results += "<div class='row justify-content-center'>";
 
       for (let i = 0; i < json.data.length; i++) {
-        results += "<div class='col-lg my-auto' align='center'>";
+        results += "<div class='col-lg my-auto img-box' align='center'>";
         results += "<img class='img-search-result' src='" + json.data[i].images.fixed_width.url + "'></img>";
+        results += "<div class='overlay'>";
+        results += "<div class='img-overlay-txt'>" + json.data[i].title + "</div>";
+        results += "</div>";
         results += "</div>";
       }
 
@@ -66,6 +74,45 @@ function loadTrending() {
       results += "</div>";
       document.getElementById("trendingResults").innerHTML = results;
       document.getElementById("trendingResults").style.display = "auto";
+
+    }).then(function() {
+      var foot = document.getElementById("footer");
+      foot.style.bottom = "auto";
+      foot.style.position = "";
+    })
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function loadRandom() {
+  document.getElementById("trendingResults").style.display = "none";
+  document.getElementById("searchResults").style.display = "none";
+
+  const urlTrending = "https://api.giphy.com/v1/gifs/random?" +
+    "api_key=" + api_key +
+    "&tag=" +
+    "&rating=pg";
+  fetch(urlTrending)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      let results = "";
+      results += "<h2 class='resultsTitle'>Random Gif</h2>";
+      results += "<div class='container gif-box'>";
+      results += "<div class='row justify-content-center'>";
+
+      results += "<div class='col-lg my-auto img-box' align='center'>";
+      results += "<img class='img-search-result' src='" + json.data.images.fixed_width.url + "'></img>";
+      results += "<div class='overlay'>";
+      let title = json.data.title;
+      if (title.length === 0) title = "Untitled";
+      results += "<div class='img-overlay-txt'>" + title + "</div>";
+      results += "</div>";
+      results += "</div>";
+
+      results += "</div>";
+      results += "</div>";
+      document.getElementById("randomResults").innerHTML = results;
+      document.getElementById("randomResults").style.display = "block";
 
     }).then(function() {
       var foot = document.getElementById("footer");
